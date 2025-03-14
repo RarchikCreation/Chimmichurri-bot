4c2e770d1ba035f1ac5315e42fbe5a958b783ae2
RarchikCreation <chimmichurri.bad@gmail.com>
2025-03-14 23:29:33 +0300

    created first structure

---
+++ cogs/protect/logic/attribute.py
@@ -0,0 +1,10 @@
+ATTRIBUTE = {
+    "name": "Название",
+    "position": "Позиция",
+    "slowmode_delay": "Медленный режим",
+    "nsfw": "NSFW",
+    "bitrate": "Битрейт",
+    "user_limit": "Ограничение пользователей",
+    "category": "Категория",
+    "permissions_synced": "Синхронизация прав"
+}
